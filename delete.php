6dd1688b477498a4a321254df27e99a6192789b7
Pritam Hirve <hiravepritam@gmail.com>
2020-08-18 21:02:22 +0530

    delete functinality
    Signed-off-by: Pritam Hirve <hiravepritam@gmail.com>

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php
+include("connection.php");
+error_reporting(0);
+$rollno = $_GET['rn'];
+
+$query="DELETE FROM STUD WHERE ROLLNO='$rollno'  ";
+$data = mysqli_query($conn, $query);
+if($data)
+{
+    echo"<script>alert('Record Deleted ')</script>";
+    ?>
+<META HTTP-EQUIV="Refresh" CONTENT="0 ; URL=http://localhost/CRUD/display.php">
+<?php
+}
+else
+{
+    echo"<font color='Red'>Sorry Failed to Delete";
+}
+?>
